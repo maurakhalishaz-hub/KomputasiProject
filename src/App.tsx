e56016fa85f48cad3ce7da5f35561a95e7d2b510
maurakhalishaz-hub <maurakhalishaz@gmail.com>
2025-11-16 20:54:33 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,22 @@ import Malang from './pages/Malang';
 function App() {
   const [currentPage, setCurrentPage] = useState<'home' | 'maluku' | 'malang' | 'bali' | 'bandung'>('home');
 
-  if (currentPage === 'maluku') return <Maluku />;
+if (currentPage === 'maluku') return <Maluku />;
 if (currentPage === 'malang') return <Malang />;
 if (currentPage === 'bali') return <Bali />;
 if (currentPage === 'bandung') return <Bandung />;
-  }
+}
 
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <HeroSection />
      <CityCards onCityClick={(city) => {
-        if (city === 'Maluku') {
-          setCurrentPage('maluku');
-        }
+  if (city === 'Maluku') setCurrentPage('maluku');
+  else if (city === 'Malang') setCurrentPage('malang');
+  else if (city === 'Bali') setCurrentPage('bali');
+  else if (city === 'Bandung') setCurrentPage('bandung');
+      
       }} />
       <TopListTable />
       <FeatureSection />
