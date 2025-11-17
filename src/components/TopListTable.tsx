929137f0ac8bbbac4781ce2355b45a8ddbafb8d6
maurakhalishaz-hub <maurakhalishaz@gmail.com>
2025-11-17 20:24:23 +0700

    Updated TopListTable.tsx

--- src/components/TopListTable.tsx
+++ src/components/TopListTable.tsx
@@ -134,7 +134,6 @@ function DestinationTable({ kota, data }) {
             ) : (
               data.map((item, index) => (
                 <tr key={index} className="border-b border-gray-200 hover:bg-gray-50 transition-colors">
-                  <td className="px-6 py-4">{kota}</td>
                   <td className="px-6 py-4">{item.kategori}</td>
                   <td className="px-6 py-4">{item.nama}</td>
                   <td className="px-6 py-4">{item.alamat}</td>
