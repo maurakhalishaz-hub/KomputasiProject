8283dcad5c33cd37307986727e4919820f591059
maurakhalishaz-hub <maurakhalishaz@gmail.com>
2025-11-17 21:09:01 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import CityCards from './components/CityCards';
-import TopListTable from './components/TopListTable';
-import FeatureSection from './components/FeatureSection';
-import Footer from './components/Footer';
-import Maluku from './pages/Maluku';
-import Bali from './pages/Bali';
-import Bandung from './pages/Bandung';
-import Malang from './pages/Malang';
+function App() {
+  const [currentPage, setCurrentPage] = useState<'home' | 'bandung' | 'malang' | 'maluku' | 'bali'>('home');
 
-const queryClient = new QueryClient();
+  if (currentPage === 'maluku') return <Maluku />;
+  if (currentPage === 'malang') return <Malang />;
+  if (currentPage === 'bali') return <Bali />;
+  if (currentPage === 'bandung') return <Bandung />;
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/bandung" element={<Bandung />} />
-          <Route path="/malang" element={<Malang />} />
-          <Route path="/maluku" element={<Maluku />} />
-          <Route path="/bali" element={<Bali />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <HeroSection />
+      <CityCards
+        onCityClick={(city) => {
+          if (city === 'Maluku') setCurrentPage('maluku');
+          else if (city === 'Malang') setCurrentPage('malang');
+          else if (city === 'Bali') setCurrentPage('bali');
+          else if (city === 'Bandung') setCurrentPage('bandung');
+        }}
+      />
+      <TopListTable />
+      <FeatureSection />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
