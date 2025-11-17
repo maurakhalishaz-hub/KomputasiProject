7fd6403d82b98d1ca84d4fcd89594d2c61aaca2f
maurakhalishaz-hub <maurakhalishaz@gmail.com>
2025-11-17 21:27:24 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import CityCards from './components/CityCards';
@@ -10,31 +10,30 @@ import Bali from './pages/Bali';
 import Bandung from './pages/Bandung';
 import Malang from './pages/Malang';
 
-function App() {
-  const [currentPage, setCurrentPage] = useState<'home' | 'bandung' | 'malang' | 'maluku' | 'bali'>('home');
-
-  if (currentPage === 'maluku') return <Maluku />;
-  if (currentPage === 'malang') return <Malang />;
-  if (currentPage === 'bali') return <Bali />;
-  if (currentPage === 'bandung') return <Bandung />;
-
-  // Ini return utama untuk halaman home
+function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <HeroSection />
-      <CityCards
-        onCityClick={(city) => {
-          if (city === 'Maluku') setCurrentPage('maluku');
-          else if (city === 'Malang') setCurrentPage('malang');
-          else if (city === 'Bali') setCurrentPage('bali');
-          else if (city === 'Bandung') setCurrentPage('bandung');
-        }}
-      />
+      <CityCards />
       <TopListTable />
       <FeatureSection />
       <Footer />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bandung" element={<Bandung />} />
+        <Route path="/malang" element={<Malang />} />
+        <Route path="/maluku" element={<Maluku />} />
+        <Route path="/bali" element={<Bali />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
